41a3850c38f251e40d1421dfb57e7258c9435b92
jatinthakur011 <jatuthakur170@gmail.com>
2026-05-13 11:27:31 +0530

    terrafrom_cloud

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.15.1"
+  cloud {
+    organization = "jatin-terraform-lab"
+    workspaces {
+      name = "terraform-aws-lab"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0273e3d83fa1b4799"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cloud-ec2"
+  }
+}
